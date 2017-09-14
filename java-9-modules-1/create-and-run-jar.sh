fcb6ed21f8d271e9708c390aa792685a9134631d
Marçal Perapoch Amadó <marcal.perapoch@gmail.com>
2017-09-14 19:34:32 +0200

    User same module name as jar

--- java-9-modules-1/create-and-run-jar.sh
+++ java-9-modules-1/create-and-run-jar.sh
@@ -10,7 +10,7 @@ mkdir libs/
 
 # Create one .jar per module inside the /libs folder
 jar --create --file libs/$MY_APP1_MODULE.jar --main-class $MY_APP1_MODULE.Main -C mods/$MY_APP1_MODULE/ .
-jar --create --file libs/second.jar -C mods/$MY_APP2_MODULE .
+jar --create --file libs/$MY_APP2_MODULE.jar -C mods/$MY_APP2_MODULE .
 
 # run MY_APP1_MODULE
 java -p libs/ -m com.myfirst.app
