18f5a9f7fe9b1c6cf01e05deccb84fda3462f85c
Marçal Perapoch Amadó <marcal.perapoch@gmail.com>
2017-09-14 19:31:38 +0200

    Created script to generate and run modules from jar files

---
+++ java-9-modules-1/create-and-run-jar.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+MY_APP1_MODULE="com.myfirst.app"
+MY_APP2_MODULE="com.mysecond.app"
+
+rm -rf libs/
+mkdir libs/
+
+# Create one .jar per module inside the /libs folder
+jar --create --file libs/$MY_APP1_MODULE.jar --main-class $MY_APP1_MODULE.Main -C mods/$MY_APP1_MODULE/ .
+jar --create --file libs/second.jar -C mods/$MY_APP2_MODULE .
+
+# run MY_APP1_MODULE
+java -p libs/ -m com.myfirst.app
+
+
+
