ff7285a377362b725b73e2cc0e0185576c736ab8
Marçal Perapoch Amadó <marcal.perapoch@gmail.com>
2017-10-24 22:46:16 +0200

    Removed unecessary dependency

--- java-9-modules-2/src/com.myapp.service/module-info.java
+++ java-9-modules-2/src/com.myapp.service/module-info.java
@@ -2,6 +2,5 @@ module com.myapp.service {
     exports com.myapp.service;
     exports com.myapp.service.provider;
     requires java.logging;
-    uses com.myapp.service.Repository;
     uses com.myapp.service.provider.RepositoryProvider;
 }
